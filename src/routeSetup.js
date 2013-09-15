73d9196ae0f1577226c6e5542396d930e43b6eeb
jeroendedauw <jeroendedauw@gmail.com>
2013-09-15 18:22:58 +0200

    Add routes from API spec

--- src/routeSetup.js
+++ src/routeSetup.js
@@ -11,5 +11,18 @@ exports.run = function(app) {
 	app.delete('/queries/:query', routes.queries.delete);
 	app.get('/queries/:query/results', routes.queries.showResults);
 
+	app.get('/results', routes.results.list);
+	app.get('/results/:result', routes.results.show);
+	app.delete('/results/:result', routes.results.delete);
+
+	app.post('/run', routes.run.run);
+	app.post('/run/:query', routes.run.runQuery);
+
+	app.get('/users/:user', routes.users.listPrivateQueries);
+	app.get('/users/:user/queries', routes.users.listPrivateQueries);
+	app.get('/users/:user/queries/public', routes.users.listPublicQueries);
+	app.get('/users/:user/results', routes.users.listPrivateResults);
+	app.get('/users/:user/results/public', routes.users.listPublicResults);
+
 	// TODO: https://github.com/JeroenDeDauw/BeyondWikidata/blob/master/ApiSpec.md
 };
