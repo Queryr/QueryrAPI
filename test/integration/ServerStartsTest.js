2c5586a6f75b6ea20b5a8fab87446e6076dc422f
jeroendedauw <jeroendedauw@gmail.com>
2013-09-15 20:03:51 +0200

    Added smoke test that sees if the home page is served

--- test/integration/ServerStartsTest.js
+++ test/integration/ServerStartsTest.js
@@ -1,6 +1,23 @@
 'use strict';
 
 var api = require('../../');
+var http = require('http');
+var server = api.server;
+
+function httpGet(url, callback) {
+	var request = http.get(url);
+	request.on("response", function(response) {
+		var receivedData = "";
+		response.setEncoding("utf8");
+
+		response.on("data", function(chunk) {
+			receivedData += chunk;
+		});
+		response.on("end", function() {
+			callback(response, receivedData);
+		});
+	});
+}
 
 exports['test'] = {
 
@@ -16,14 +33,26 @@ exports['test'] = {
 		// This ensures the log function is getting called at least once.
 		server.start( function() {
 			test.ok(true);
+			server.stop();
 			test.done();
 		} );
+	},
 
-		// TODO: assert is serving
+	'test can get home page': function(test) {
+		test.expect(2);
 
-		server.stop();
+		server.start();
 
-		// TODO: assert stopped serving
+		httpGet("http://localhost:3000", function(response, receivedData) {
+			var foundHomePage = receivedData.indexOf("Welcome to") !== -1;
+			test.ok(foundHomePage, "home page should contain the test marker");
+
+			var noBackdoor = receivedData.indexOf("backdoor") === -1;
+			test.ok(noBackdoor, "home page should not contain any backdoor");
+
+			server.stop();
+			test.done();
+		});
 	}
 
 };
